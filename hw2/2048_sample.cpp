17ca4a036c34a49106b2927ac2cce7d0f53b4da0
Yelloooowww <cococing0324@gmail.com>
2021-04-11 08:32:41 +0800

    5-tuple shape='T'

--- hw2/2048_sample.cpp
+++ hw2/2048_sample.cpp
@@ -890,30 +890,20 @@ int main(int argc, const char* argv[]) {
 	std::srand(seed);
 
 	// initialize the features
-	// tdl.add_feature(new pattern({ 0, 1, 2, 3, 4, 5 }));
-	// tdl.add_feature(new pattern({ 4, 5, 6, 7, 8, 9 }));
-	// tdl.add_feature(new pattern({ 0, 1, 2, 4, 5, 6 }));
-	// tdl.add_feature(new pattern({ 4, 5, 6, 8, 9, 10 }));
-	tdl.add_feature(new pattern({ 0, 1, 2, 3 }));
-	tdl.add_feature(new pattern({ 4, 5, 6, 7 }));
-	tdl.add_feature(new pattern({ 8, 9, 10, 11 }));
-	tdl.add_feature(new pattern({ 12, 13, 14, 15 }));
-	tdl.add_feature(new pattern({ 0, 4, 8, 12 }));
-	tdl.add_feature(new pattern({ 1, 5, 9, 13 }));
-	tdl.add_feature(new pattern({ 2, 6, 10, 14 }));
-	tdl.add_feature(new pattern({ 3, 7, 11, 15 }));
+	tdl.add_feature(new pattern({ 0, 1, 2, 5, 9 }));
+	tdl.add_feature(new pattern({ 1, 2, 3, 6, 10 }));
+	tdl.add_feature(new pattern({ 4, 5, 6, 9, 13 }));
+	tdl.add_feature(new pattern({ 5, 6, 7, 10, 14 }));
 
-	tdl.add_feature(new pattern({ 0, 1, 4, 5 }));
-	tdl.add_feature(new pattern({ 1, 2, 5, 6 }));
-	tdl.add_feature(new pattern({ 2, 3, 6, 7 }));
+	tdl.add_feature(new pattern({ 0, 4, 5, 6, 8 }));
+	tdl.add_feature(new pattern({ 3, 5, 6, 7, 11 }));
+	tdl.add_feature(new pattern({ 4, 8, 9, 10, 12 }));
+	tdl.add_feature(new pattern({ 7, 9, 10, 11, 15 }));
 
-	tdl.add_feature(new pattern({ 4, 5, 8, 9 }));
-	tdl.add_feature(new pattern({ 5, 6, 9, 10 }));
-	tdl.add_feature(new pattern({ 6, 7, 10, 11 }));
-
-	tdl.add_feature(new pattern({ 8, 9, 12, 13 }));
-	tdl.add_feature(new pattern({ 9, 10, 13, 14 }));
-	tdl.add_feature(new pattern({ 10, 11, 14, 15 }));
+	tdl.add_feature(new pattern({ 1, 5, 8, 9, 10 }));
+	tdl.add_feature(new pattern({ 2, 6, 9, 10, 11 }));
+	tdl.add_feature(new pattern({ 5, 9, 12, 13, 14 }));
+	tdl.add_feature(new pattern({ 6, 10, 13, 14, 15 }));
 
 	// restore the model from file
 	tdl.load("");
