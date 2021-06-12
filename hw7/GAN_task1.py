cce7e68fae6ec39a216b899a81b1e426688c6936
yelloooowww <cococing0324@gmail.com>
2021-06-12 22:12:38 +0800

    condition GAN

--- hw7/GAN_task1.py
+++ hw7/GAN_task1.py
@@ -27,7 +27,7 @@ import copy
 from torch.utils.tensorboard import SummaryWriter
 
 
-batchSize = 1
+batchSize = 16
 nz=110
 num_classes=24
 epochs = 200
@@ -48,21 +48,20 @@ if __name__=='__main__':
     optimizerD = optim.Adam(netD.parameters(), lr=0.0002, betas=(0.5, 0.999))
     optimizerG = optim.Adam(netG.parameters(), lr=0.0002, betas=(0.5, 0.999))
     # loss functions
-    dis_criterion = nn.BCELoss()
-    aux_criterion = nn.BCELoss()
-    # tensor placeholders
-    input = torch.FloatTensor(batchSize, 3, 64, 64).to(device)
-    with torch.no_grad():
-        noise = torch.FloatTensor(batchSize, nz, 1, 1).to(device)
-        dis_label = torch.FloatTensor(batchSize).to(device)
-        aux_label = torch.FloatTensor(batchSize).to(device)
+    dis_criterion = nn.BCELoss().to(device)
+    aux_criterion = nn.BCELoss().to(device)
+
     real_label = 1
     fake_label = 0
     best_score = 0
+    iter = 0
     for epoch in range(1,epochs+1):
         netG.train()
         netD.train()
-        for i, (images,conditions) in enumerate(loader_train):
+        for i, (images,read_conditions) in enumerate(loader_train):
+            iter += 1
+            conditions = Variable(read_conditions)
+
 
             ############################
             # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
@@ -72,28 +71,34 @@ if __name__=='__main__':
             batch_size = len(images)
 
             predict_realfake, predict_classes = netD(images.to(device)) # return realfake, classes
-            with torch.no_grad(): dis_label.data.fill_(real_label)
+            with torch.no_grad():
+                dis_label = torch.FloatTensor(batchSize).to(device)
+                dis_label.data.fill_(real_label)
             dis_errD_real = dis_criterion(predict_realfake, dis_label)
-            aux_errD_real = aux_criterion(predict_classes, conditions.to(device))
+            aux_label = copy.deepcopy(conditions)
+            aux_label = aux_label.to(device)
+            aux_errD_real = aux_criterion(predict_classes, aux_label)
             errD_real = dis_errD_real + aux_errD_real
             errD_real.backward(retain_graph=True)
-            optimizerD.step()
 
             # train with fake
             with torch.no_grad():
+                noise = torch.FloatTensor(batch_size, nz, 1, 1)
+                noise = Variable(noise)
                 noise.data.resize_(batch_size, nz, 1, 1).normal_(0, 1)
                 label = np.random.randint(0, num_classes, batch_size)
                 noise_ = np.random.normal(0, 1, (batch_size, nz))
-                class_onehot = np.zeros((batch_size, num_classes))
-                class_onehot[np.arange(batch_size), label] = 1
+                class_onehot = copy.deepcopy(conditions)
                 noise_[np.arange(batch_size), :num_classes] = class_onehot[np.arange(batch_size)]
                 noise_ = (torch.from_numpy(noise_))
                 noise.data.copy_(noise_.view(batch_size, nz, 1, 1))
+
+                dis_label = torch.FloatTensor(batchSize).to(device)
                 dis_label.data.resize_(batch_size).fill_(fake_label)
-            fake = netG(noise)
+            fake = netG(noise.to(device))
             predict_realfake, predict_classes = netD(fake)
             dis_errD_fake = dis_criterion(predict_realfake, dis_label)
-            aux_errD_fake = aux_criterion(predict_classes, conditions.to(device))
+            aux_errD_fake = aux_criterion(predict_classes, aux_label)
             errD_fake = dis_errD_fake + aux_errD_fake
             errD_fake.backward(retain_graph=True)
             optimizerD.step()
@@ -102,49 +107,47 @@ if __name__=='__main__':
             # (2) Update G network: maximize log(D(G(z)))
             ###########################
             netG.zero_grad()
-            with torch.no_grad(): dis_label.data.resize_(batch_size).fill_(real_label)  # fake labels are real for generator cost
+            with torch.no_grad():
+                dis_label = torch.FloatTensor(batchSize).to(device)
+                dis_label.data.resize_(batch_size).fill_(real_label)  # fake labels are real for generator cost
             predict_realfake, predict_classes = netD(fake)
             dis_errG = dis_criterion(predict_realfake, dis_label)
-            aux_errG = aux_criterion(predict_classes, conditions.to(device))
+            aux_errG = aux_criterion(predict_classes, aux_label)
             errG = dis_errG + aux_errG
             errG.backward(retain_graph=True)
             optimizerG.step()
 
-            if i % 50 == 0: print(i, errD_fake.item(),errD_real.item(),errG.data.item())
+            # evaluate
+            netG.eval()
+            netD.eval()
+            with torch.no_grad():
+                test_conditions = get_test_conditions()
+                noise = torch.FloatTensor(batchSize, nz-num_classes, 1, 1).to(device)
+                c_resize = copy.deepcopy(test_conditions)
+                c_resize.resize_( (batchSize, num_classes, 1, 1) )
+                z = torch.cat((noise.to(device),c_resize.to(device)),dim=1)
+
+            gen_imgs=netG(z)
+            score = evaluation_model.eval(gen_imgs,test_conditions)
+
+            if score > best_score :
+                best_G = copy.deepcopy(netG.state_dict())
+                best_D = copy.deepcopy(netD.state_dict())
+                torch.save(best_G,'models/iter_'+ str(iter)+'_'+str(score)+'_'+'_G.pt')
+                torch.save(best_D,'models/iter_'+ str(iter)+'_'+str(score)+'_'+'_D.pt')
+                print('save: models/iter_'+ str(iter)+'_'+str(score)+'_'+'_G.pt')
+                best_score = score
+
+            writer.add_scalars("cGAN_task1_Train", {    "errD_fake":errD_fake.item(),\
+                                                        "errD_real": errD_real.item(),\
+                                                        "errG": errG.item(),\
+                                                        "score": score  }, iter)
+            writer.flush()
+            if i % 10 == 0: print(i, errD_fake.item(),errD_real.item(),errG.data.item(),'score = ',score)
+        
 
 
 
-        # evaluate
-        netG.eval()
-        netD.eval()
-        with torch.no_grad():
-            noise = torch.FloatTensor(1, nz, 1, 1).to(device)
-            noise.data.resize_(1, nz, 1, 1).normal_(0, 1)
-            label = np.random.randint(0, num_classes, 1)
-            noise_ = np.random.normal(0, 1, (1, nz))
-            class_onehot = np.zeros((1, num_classes))
-            class_onehot[np.arange(1), label] = 1
-            noise_[np.arange(1), :num_classes] = class_onehot[np.arange(1)]
-            noise_ = (torch.from_numpy(noise_))
-            noise.data.copy_(noise_.view(1, nz, 1, 1))
-            gen_imgs=netG(noise)
-        test_conditions = get_test_conditions()
-        score = evaluation_model.eval(gen_imgs,test_conditions)
-        test_conditions_new = get_test_conditions_new()
-        score_new = evaluation_model.eval(gen_imgs,test_conditions)
-        print('epoch:',epoch,'score:',score,'score_new:',score_new)
-
-        best_G = copy.deepcopy(netG.state_dict())
-        best_D = copy.deepcopy(netD.state_dict())
-        torch.save(best_G,'models/'+ str(epoch)+'_'+str(score)+'_'+str(score_new)+'_G.pt')
-        torch.save(best_D,'models/'+ str(epoch)+'_'+str(score)+'_'+str(score_new)+'_D.pt')
 
         # savefig
         save_image(gen_imgs, os.path.join('results', f'epoch{epoch}.png'), nrow=8, normalize=True)
-
-        writer.add_scalars("cGAN_task1_Train", {    "errD_fake":errD_fake.item(),\
-                                                    "errD_real": errD_real.item(),\
-                                                    "errG": errG.item(),\
-                                                    "score": score,\
-                                                    "score_new": score_new}, epoch)
-        writer.flush()
